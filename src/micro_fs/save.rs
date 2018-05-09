462279e37e9f4b3de712401fec076240d4acf35c
orpheeantoniadis <orphee.antoniadis@gmail.com>
2018-05-10 00:25:57 +0200

    forgot to uncomment a part of code

--- src/micro_fs/save.rs
+++ src/micro_fs/save.rs
@@ -12,14 +12,13 @@ impl MicroFS {
             image.seek(SeekFrom::Start(SECTOR_SIZE as u64)).expect("File seek failed !");
             image.write_all(&(self.fat)).expect("Failed to write in file!");
             
-            // let size = metadata(self.image.clone()).expect("Failed getting metadata!").len() as usize;
-            // let rest = (size / SECTOR_SIZE) - (self.sb.fat_size as usize + 1);
-            // for _i in 0..rest {
-            //     image.write_all(&[0;SECTOR_SIZE]).expect("Failed to write in file!");
-            // }
-            // 
-            // image.seek(SeekFrom::Start(self.root_entry() as u64)).expect("File seek failed !");
+            let size = metadata(self.image.clone()).expect("Failed getting metadata!").len() as usize;
+            let rest = (size / SECTOR_SIZE) - (self.sb.fat_size as usize + 1);
+            for _i in 0..rest {
+                image.write_all(&[0;SECTOR_SIZE]).expect("Failed to write in file!");
+            }
             
+            image.seek(SeekFrom::Start(self.root_entry() as u64)).expect("File seek failed !");
             for entry in self.entries.clone() {
                 image.write_all(&(entry.name)).expect("Failed to write in file!");
                 image.write_all(&(mem::transmute::<u16, [u8; 2]>(entry.start))).expect("Failed to write in file!");
